30061bd5e59ad6450a38902ccc3fb528e9fe2fbb
phillipphan <itsphillipphan@gmail.com>
2021-01-14 13:50:20 -0600

    catalog displays table

--- src/scripts/catalog.js
+++ src/scripts/catalog.js
@@ -1,9 +1,21 @@
 export const catalog = (harvestArr) => {
-  const contentElement = document.querySelector(".container")
+  const contentElement = document.querySelector(".plantTable")
+
   let catalogHTMLRepresentation = ""
+  let plantCounter = {}
+
   for (const plant of harvestArr) {
-    catalogHTMLRepresentation += `<section class="plant">${plant.type}</section>`
+    if (!(plant.type in plantCounter)) {
+      plantCounter[plant.type] = 1
+    } else if (plant.type in plantCounter) {
+      plantCounter[plant.type] += 1
+    }
   }
+
+  for (const key of Object.keys(plantCounter)) {
+    catalogHTMLRepresentation += `<tr><td>${key}</td><td>${plantCounter[key]}</td></tr>`
+  }
+
   contentElement.innerHTML += `
       ${catalogHTMLRepresentation}
   `
